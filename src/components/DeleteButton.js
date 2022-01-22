fa298e53f706400b4025ad3ba75e445152accd3a
Dmytro Zozulia <badgerwannabe@gmail.com>
2022-01-22 09:35:53 +0100

    Update DeleteButton.js
    updated DeleteButton to work for traninings too

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -3,30 +3,47 @@ import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import { Button, Confirm, Icon } from "semantic-ui-react";
-import { FETCH_TRAINERS_QUERY } from "../utils/graphql";
+import { FETCH_TRAINERS_QUERY, FETCH_TRAININGS_QUERY } from "../utils/graphql";
 
-function DeleteButton({ trainerId, callback }) {
+function DeleteButton({ trainerId, trainingId, callback }) {
   const [confirmOpen, setConfirmOpen] = useState(false);
-  const mutation = DELETE_TRAINER_MUTATION;
+  const mutation = trainerId
+    ? DELETE_TRAINER_MUTATION
+    : DELETE_TRAINING_MUTATION;
+
   const [deleteElement] = useMutation(mutation, {
     update(proxy) {
       setConfirmOpen(false);
 
-      const data = proxy.readQuery({
-        query: FETCH_TRAINERS_QUERY,
-      });
+      if (!trainingId) {
+        const data = proxy.readQuery({
+          query: FETCH_TRAINERS_QUERY,
+        });
 
-      proxy.writeQuery({
-        query: FETCH_TRAINERS_QUERY,
-        data: {
-          getTrainers: data.getTrainers.filter((p) => p.id !== trainerId),
-        },
-      });
+        proxy.writeQuery({
+          query: FETCH_TRAINERS_QUERY,
+          data: {
+            getTrainers: data.getTrainers.filter((p) => p.id !== trainerId),
+          },
+        });
+      } else {
+        const data = proxy.readQuery({
+          query: FETCH_TRAININGS_QUERY,
+        });
+
+        proxy.writeQuery({
+          query: FETCH_TRAININGS_QUERY,
+          data: {
+            getTrainings: data.getTrainings.filter((p) => p.id !== trainingId),
+          },
+        });
+      }
 
       if (callback) callback();
     },
     variables: {
       trainerId,
+      trainingId,
     },
   });
 
@@ -53,6 +70,11 @@ function DeleteButton({ trainerId, callback }) {
 const DELETE_TRAINER_MUTATION = gql`
   mutation deleteTrainer($trainerId: ID!) {
     deleteTrainer(trainerId: $trainerId)
+  }
+`;
+const DELETE_TRAINING_MUTATION = gql`
+  mutation deleteTraining($trainingId: ID!) {
+    deleteTraining(trainingId: $trainingId)
   }
 `;
 
