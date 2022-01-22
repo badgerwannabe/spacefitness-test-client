1370af8ecb428164d3e0038735ea9aa00326c077
Dmytro Zozulia <badgerwannabe@gmail.com>
2022-01-22 10:21:47 +0100

    Update hooks.js
    fixed not initializing the "result" var

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 export const useForm = (callback, initialState={})=>{
     const [values, setValues] = useState(initialState);
 
-    const onChange = (event)=>{
+    const onChange = (event, result)=>{
         const { name, value } = result || event.target;
         setValues({ ...values, [name]: value });
     };
