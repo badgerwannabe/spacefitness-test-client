7a839777d5b74d066db7807846c92b90c8fd580f
Dmytro Zozulia <badgerwannabe@gmail.com>
2022-01-22 10:17:43 +0100

    Update hooks.js
    updated to be able to edit details in forms

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -1,13 +1,11 @@
 import {useState} from 'react'
 
-
-
 export const useForm = (callback, initialState={})=>{
     const [values, setValues] = useState(initialState);
 
     const onChange = (event)=>{
-        setValues({...values, [event.target.name]: event.target.value})
-        console.log({...values, [event.target.name]: event.target.value})
+        const { name, value } = result || event.target;
+        setValues({ ...values, [name]: value });
     };
     
     const onSubmit = (event) =>{
