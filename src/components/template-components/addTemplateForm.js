665ba17ed3f8155fba7d12071b46aa9d8ce3da06
Dmytro Zozulia <dm.zozulia@gmail.com>
2022-02-03 22:44:43 +0000

    on button click component adds two fields, they are connected to state and values persist even when another set of fields is added

--- src/components/template-components/addTemplateForm.js
+++ src/components/template-components/addTemplateForm.js
@@ -13,11 +13,13 @@ function AddTemplateForm (props){
         date:'', 
         // dayTrainings:[]
     });
+  //addTraningObject State 
+    const [trainingObjects, setTrainingObjects] = useState([]);
 
 //apollo hook to send data through GraphQL    
 const [createDay, {error}] = useMutation(ADD_TEMPLATES_MUTATION, {
     errorPolicy: 'all',
-    variables:values, 
+     variables:{values,trainingObjects},
     update(proxy, result){
         const data = proxy.readQuery({ 
             query:FETCH_TEMPLATES_QUERY,
@@ -36,8 +38,7 @@ const [createDay, {error}] = useMutation(ADD_TEMPLATES_MUTATION, {
         createDay();
     }
 
-    //addTraningObject State 
-    const [trainingObjects, setTrainingObjects] = useState([]);
+  
 
 
 
@@ -47,32 +48,50 @@ const [createDay, {error}] = useMutation(ADD_TEMPLATES_MUTATION, {
       let newCount = trainingObjects.length + 1;
       console.log(newCount)
         const newTrainingObject =  {
-              time:"testDate",
-              trainingInfo:"testingString",
+              time:"",
+              training:"",
               id: newCount
               };
       
          setTrainingObjects([...trainingObjects, newTrainingObject]);
     }
 
-    function updateObjectValues(event, id){
-      const {name,value } = event.target
-      console.log(name + " " + value)
+
+
+    function updateObjectValues(event, index){
+
+      const {name,value} = event.target;
+      // console.log(name + " " + value + " "+id)
+
+
+      // const todoIndex = trainingObjects.findIndex((obj) => obj.id === id);
+
+      let values = [...trainingObjects]
+
+      values[index][name] = value
+      // let newObj = values[todoIndex]
+      // console.log(newObj)
+      // let updObj = {...newObj, [name]: value}
+      // console.log(updObj)
+      // values[todoIndex] = updObj
+
+      setTrainingObjects(values)
+
       console.table(trainingObjects)
+  // fix state from updating not to lose values
+  // send the values through form 
 
-      let updatedTraining = trainingObjects.filter(item=>{
-        return(
-        item.id === id
-        )
-      })
-
-    console.log(updatedTraining)
     }
+  
 
 
   //little component I want to dynamically add each time people press a button  
-  const AddDayTraining =({trainingObject,trainingItemIndex}) =>{
+  const AddDayTraining =({trainingObject, trainingItemIndex}) =>{
 // console.log(trainingObject)
+
+  let isThat = trainingObjects.find( item => 
+                   item.id === trainingObject.id)
+
     return(
       <>
       <Form.Field>
@@ -84,14 +103,14 @@ const [createDay, {error}] = useMutation(ADD_TEMPLATES_MUTATION, {
                 //  console.log(event.target.value)
                 (event) => updateObjectValues(event, trainingItemIndex)
                }
-               values={values.time}
+                value={trainingObject.time}
                error={error ? true : false}
                />
             <Form.Input
                placeholder="training"
                name="training"
-                 onChange={ (event) => updateObjectValues(event, trainingItemIndex)}
-               values={values.training}
+               onChange={ (event) => updateObjectValues(event, trainingItemIndex)}
+               value={trainingObject.training}
                error={error ? true : false}
                />
        </Form.Field>
@@ -117,7 +136,7 @@ const [createDay, {error}] = useMutation(ADD_TEMPLATES_MUTATION, {
                  <Button type="button" onClick={addTraining}>Add training</Button>
                   </Form.Field>
 
-                  <div className='your list of training things'>
+                  <div className='list of trainings'>
                     {trainingObjects.length === 0 ? "" : trainingObjects.map((trainingObject, index) => (
                     <AddDayTraining trainingObject={trainingObject} trainingItemIndex={index} key={index}/>
                 
