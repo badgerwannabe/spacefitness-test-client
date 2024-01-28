c4b83bf5c0d340133cb2f00a67225401932208fa
Dmytro Zozulia <dm.zozulia@gmail.com>
2024-01-28 17:15:23 +0100

    changed server uri
    changed server uri from Heroku to Replit temporarily

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,8 +4,9 @@ import {ApolloClient,InMemoryCache,createHttpLink,ApolloProvider} from '@apollo/
 
 
 const httpLink = createHttpLink({
-    uri: 'https://spacefit-server-replit.herokuapp.com/graphql'
+    // uri: 'https://spacefit-server-replit.herokuapp.com/graphql'
     // uri: 'https://enl2byybqrqyvm9.m.pipedream.net'
+    uri: 'https://94897727-4608-4d6f-b9fc-bd012d0a7023-00-1zugykz0p5v91.kirk.replit.dev/'
    
 })
 
